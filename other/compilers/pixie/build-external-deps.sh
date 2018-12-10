02c932cf8c174f8e81d87df8f0799c74e07e5865
Florin65 <florin@lunar-linux.org>
2018-12-10 21:41:27 +0200

    Remove some not functional modules.

--- other/compilers/pixie/build-external-deps.sh
+++
@@ -1,9 +0,0 @@
-mkdir -p externals
-tar -zxf libuv-1.8.0.tar.gz
-cd libuv-1.8.0
-sh autogen.sh
-./configure --prefix=/home/florin/projects/externals
-make
-make install
-cd ..
-tar -cjf `uname -s`-`uname -m`.tar.bz2 externals/*
